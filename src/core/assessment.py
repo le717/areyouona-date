92bc5a444dad7d40daaf3a90784522078cbda369
Caleb Ely <le717.code@yahoo.com>
2018-07-28 12:46:31 -0400

    Use math.ceil reported accuracy for Yelp API accuracy

--- src/core/assessment.py
+++ src/core/assessment.py
@@ -8,11 +8,11 @@ __RESPONSE_YES = "yes"
 
 
 def make(user_details: dict) -> str:
+    # Construct the required request parameters
     url_params = {
         "latitude": user_details["lat"],
         "longitude": user_details["lng"],
-        # "radius": user_details["acc"],
-        "radius": 200,  # TODO Exact val TBD, see gh-7
+        "radius": math.ceil(user_details["acc"]),
         "categories": "restaurants,hotdogs",
         "locale": "en_US",
         "limit": 10,
