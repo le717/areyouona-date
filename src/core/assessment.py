dbcca3cebb4f5ca4686e2e5f9c76c23704e70a13
Caleb Ely <le717.code@yahoo.com>
2018-07-28 20:50:33 -0400

    Toss out requests with an accuracy value >= 1000 meters (0.6 miles)

--- src/core/assessment.py
+++ src/core/assessment.py
@@ -1,13 +1,20 @@
 __all__ = ["make"]
 
 
-# Possible assessment responses
+# Common values used in making an assessment
 __RESPONSE_NO = "no"
 __RESPONSE_MAYBE = "maybe"
 __RESPONSE_YES = "yes"
+__ABSURD_ACCURACY_THRESHOLD = 1000
 
 
 def make(user_details: dict) -> str:
+    # The Geolocation API did not respond with good data
+    # (aka an absurd accuracy meter distance) so we can't use that data
+    # https://developer.mozilla.org/en-US/docs/Web/API/Coordinates)
+    if user_details["acc"] >= __ABSURD_ACCURACY_THRESHOLD:
+        return __RESPONSE_NO
+
     # Construct the required request parameters
     url_params = {
         "latitude": user_details["lat"],
